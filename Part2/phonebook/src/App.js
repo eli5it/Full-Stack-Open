ef7e597cf4ebf252d05c5e830f6d564324e6ec79
Eli Davis <eli.davis1104@gmail.com>
2022-03-03 16:28:32 -0500

    fixed

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,8 +1,9 @@
 import { eventWrapper } from "@testing-library/user-event/dist/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import PersonsForm from "./components/PersonsForm";
 import Filter from "./components/Filter";
+import axios from "axios";
 
 const DisplayItem = ({ name, number }) => (
   <div>
@@ -10,16 +11,18 @@ const DisplayItem = ({ name, number }) => (
   </div>
 );
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
   const [filteredPersons, setFilteredPersons] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setFilteredPersons(response.data);
+    });
+  }, []);
 
   const filterPerson = (name, filter) => {
     return name.slice(0, filter.length).toUpperCase() === filter.toUpperCase();
