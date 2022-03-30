caa7d3da1c54ae984446acfb1e06a0fbbf1659ab
Eli Davis <eli.davis1104@gmail.com>
2022-03-30 18:58:40 -0400

    finished exercise 4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const app = express(); // the actual Express application
 const mongoose = require('mongoose');
+
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
 const logger = require('./utils/logger');
 mongoose
   .connect(config.MONGODB_URI)
@@ -20,5 +23,6 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
