8c8f5bd20f33bea60942d642b8b684f2cf3eb01b
Eli Davis <eli.davis1104@gmail.com>
2022-03-30 23:55:45 -0400

    finished exercise 4.22

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -10,12 +10,10 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-
-  if (!decodedToken.id) {
+  const user = request.user;
+  if (!user) {
     return response.status(401).json({ error: 'token missing or invalid' });
   }
-  const user = await User.findById(decodedToken.id);
   if (!body.title || !body.author) {
     return response.status(400).end();
   }
@@ -25,20 +23,21 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     title: body.title,
-    user: user._id,
+    user: request.user._id,
   });
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(blog._id);
+  console.log(user.blogs);
   await user.save();
   response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   const blogToDelete = await Blog.findById(request.params.id);
-  if (blogToDelete.user.toString() === decodedToken.id) {
+  const user = request.user;
+  if (blogToDelete.user.toString() === user._id.toString()) {
     blogToDelete.remove();
-    response.status(204).end();
+    return response.status(204).end();
   }
   return response.status(400).json({ error: 'token missing or invalid' });
 });
