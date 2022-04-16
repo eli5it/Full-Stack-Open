761bc9d8c6f6985b12d323221ef5afacdd10e6bf
Eli Davis <eli.davis1104@gmail.com>
2022-04-16 11:13:53 -0400

    fixed exercise 6.8

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,7 +4,6 @@ import AnecdoteList from './components/AnecdoteList';
 const App = () => {
   return (
     <div>
-      <h2>create new</h2>
       <AnecdoteForm />
       <h2>Anecdotes</h2>
       <AnecdoteList />
