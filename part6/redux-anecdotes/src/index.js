bbf1c441e60dc8c944f437e85e57b4754b39e254
Eli Davis <eli.davis1104@gmail.com>
2022-04-18 01:00:41 -0400

    finished exercise 6.13

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { createStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import App from './App';
-import anecdoteReducer from './reducers/anecdoteReducer';
+import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer';
 import notificationReducer from './reducers/notificationReducer';
 import filterReducer from './reducers/filterReducer';
 
