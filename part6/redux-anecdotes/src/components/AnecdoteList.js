38cdaf668c0e4e832942435a80b9bb9f5190e17e
Eli Davis <eli.davis1104@gmail.com>
2022-04-17 22:44:37 -0400

    finished exercise 6.12

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -6,7 +6,16 @@ import {
 } from '../reducers/notificationReducer';
 const AnecdoteList = () => {
   const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state.anecdotes);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    if (filter) {
+      return anecdotes.filter(
+        (anecdote) =>
+          anecdote.content.slice(0, filter.length).toUpperCase() ===
+          filter.toUpperCase()
+      );
+    }
+    return anecdotes;
+  });
   const vote = (id, content) => {
     dispatch(voteOnAnecdote(id));
     dispatch(displayVote(content));
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { createStore } from '@reduxjs/toolkit';
 import App from './App';
 import anecdoteReducer from './reducers/anecdoteReducer';
 import notificationReducer from './reducers/notificationReducer';
+import filterReducer from './reducers/filterReducer';
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  notification: notificationReducer,
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+    notification: notificationReducer,
+  },
 });
-
-const store = createStore(reducer);
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
